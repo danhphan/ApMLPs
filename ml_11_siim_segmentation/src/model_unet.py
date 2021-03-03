26c5cd169854f2729296727acda04b2bf6a46466
danhphan <danh.phan.mq@gmail.com>
2021-03-03 17:52:35 +1100

    create UNet for image segmentation

--- ml_11_siim_segmentation/src/model_unet.py
+++ ml_11_siim_segmentation/src/model_unet.py
@@ -19,4 +19,80 @@ def double_conv(in_channels, out_channels):
     return conv
 
 def crop_tensor(tensor, target_tensor):
+    """
+    Center crops a tensor to size of a given target tensor size
+    Both tensors are of shape (bs, c, h, w)
+    :param tensor: a tensor that needs to be cropped
+    :param target_tensor: target tensor of smaller size
+    :return: cropped tensor
+    """
+    target_size = target_tensor.size()[2]
+    tensor_size = tensor.size()[2]
+    delta = tensor_size - target_size
+    delta = delta // 2
+    return tensor[:, :, delta:tensor_size-delta, delta:tensor_size-delta]
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.down_conv_1 = double_conv(1, 64)
+        self.down_conv_2 = double_conv(64, 128)
+        self.down_conv_3 = double_conv(128, 256)
+        self.down_conv_4 = double_conv(256, 512)
+        self.down_conv_5 = double_conv(512, 1024)
+
+        self.up_trans_1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
+        self.up_conv_1  = double_conv(1024, 512)
+        self.up_trans_2 = nn.ConvTranspose2d(in_channels=512, out_channels=1024, kernel_size=2, stride=2)
+        self.up_conv_2  = double_conv(512, 256)
+        self.up_trans_3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+        self.up_conv_3  = double_conv(256, 128)
+        self.up_trans_4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+        self.up_conv_4  = double_conv(128, 64)
+        self.out = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1)
+
+    def forward(self, image):
+        # Encoder
+        x1 = self.down_conv_1(image)
+        x2 = self.max_pool(x1)
+        x3 = self.down_conv_2(x2)
+        x4 = self.max_pool(x3)
+        x5 = self.down_conv_3(x4)
+        x6 = self.max_pool(x5)
+        x7 = self.down_conv_4(x6)
+        x8 = self.max_pool(x7)
+        x9 = self.down_conv_5(x8)
+        # Decoder
+        x = self.up_trans_1(x9)
+        y = crop_tensor(x7, x)
+        x = self.up_conv_1(torch.cat([x, y], axis=1))
+
+        x = self.up_trans_2(x)
+        y = crop_tensor(x5, x)
+        print(x.shape, y.shape)
+        x = self.up_conv_2(torch.cat([x, y], axis=1))
+    
+        x = self.up_trans_3(x)
+        y = crop_tensor(x3, x)
+        x = self.up_conv_3(torch.cat([x, y], axis=1))
+    
+        x = self.up_trans_4(x)
+        y = crop_tensor(x1, x)
+        x = self.up_conv_4(torch.cat([x, y], axis=1))
+        # Output
+        out = self.out(x)
+        return out
+
+if __name__ == "__main__":
+
+    t1 = torch.rand((1, 1, 512, 512))
+    t2 = torch.rand((1, 1, 200, 200))
+    t3 = crop_tensor(t1, t2)
+    print(t1.shape, t2.shape, t3.shape)
+
+    image = torch.rand((1, 1, 572, 572))
+    model = UNet()
+    print(model(image))
     
